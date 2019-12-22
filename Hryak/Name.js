dc3d5971b18d232bf35a50581af694ab83691209
SergeyBocharov <59083438+SergeyBocharov@users.noreply.github.com>
2019-12-22 15:23:25 +0500

    Update Name.js

--- Hryak/Name.js
+++ Hryak/Name.js
@@ -14,7 +14,7 @@
 final Context context = Context.enter(); 
 try { final ScriptableObject scope = context.initStandardObjects(); 
      ScriptableObject.defineClass(scope, Target.class, false, true); 
-     context.evaluateString(scope, script, "script", 1, null); // etc. } finally { Context.exit(); }
+     context.evaluateString(scope, script, "script", 1, null); 
 
 
 var RussianNameProcessor = {
@@ -288,3 +288,4 @@ RussianName.prototype = {
 		return RussianNameProcessor.word(this.mn, this.sex, 'middleName', gcase);
 	}
 }
+etc. } finally { Context.exit(); }
