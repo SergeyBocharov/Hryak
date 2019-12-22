bbe8c4ca84e1b3939ee472cc17550eff25cc26e8
SergeyBocharov <59083438+SergeyBocharov@users.noreply.github.com>
2019-12-22 15:21:25 +0500

    Update Name.js

--- Hryak/Name.js
+++ Hryak/Name.js
@@ -11,7 +11,10 @@
  * @version  0.1.6
  * @author   Johnny Woo <agalkin@agalkin.ru>
  */
-
+final Context context = Context.enter(); 
+try { final ScriptableObject scope = context.initStandardObjects(); 
+     ScriptableObject.defineClass(scope, Target.class, false, true); 
+     context.evaluateString(scope, script, "script", 1, null); // etc. } finally { Context.exit(); }
 
 
 var RussianNameProcessor = {
